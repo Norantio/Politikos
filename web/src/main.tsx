f27a6fdf13718af06200d430b3b3c8442b318358
Osidenn <nicholas.wittkamp@gmail.com>
2026-05-12 09:58:23 -0600

    fix(web): add vite types reference to main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -13,3 +14,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
+
